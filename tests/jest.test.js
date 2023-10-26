c3e2c9ce32b9fe0d7d84bb538a4262f70a552002
masaoskari <masaoskari.linna@gmail.com>
2023-10-26 14:47:01 +0300

    jest tests refactor

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -11,4 +11,9 @@ describe('sum function', () => {
       .not.toBeLessThan(3)
       .not.toEqual(0)
   });
+
+  test('adds -1 + 2 to equal 1', () => {
+    expect(sum(-1, 2))
+      .toBe(1)
+  });
 });
