f8dc14934b5f02137abf9a10843cb39d4173f078
masaoskari <masaoskari.linna@gmail.com>
2023-10-26 14:11:08 +0300

    mocha tests implemented

--- tests/test_mocha.js
+++ tests/test_mocha.js
@@ -7,15 +7,4 @@ describe('sum', function () {
     assert.equal(result, 8);
   });
 
-  it('should handle negative numbers', function () {
-    const result = sum(-3, 5);
-    assert.equal(result, 2);
-  });
-
-  it('should handle zero values', function () {
-    const result = sum(0, 0);
-    assert.equal(result, 0);
-  });
-
-  // Add more test cases as needed to thoroughly test the function
 });
